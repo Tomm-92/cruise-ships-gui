434aaa076af910a85b49adaa67d88c5d7996ffac
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-01-15 18:11:03 +0000

    sail button working

--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,10 @@
   class Controller {
     constructor(ship) {
       this.ship = ship;
-      this.initialiseSea()         
+      this.initialiseSea();
+      document.querySelector('#sailButton').addEventListener('click', () => {
+        this.setSail();
+      });        
     }
 
     initialiseSea() {
@@ -33,14 +36,38 @@
     //this for each works by iterating over the ports passed in to the renderPorts function and creates a new div which is added to the DOM (can see in browser console)
     // the newPortElement is given a class name port and the data tags are then that ports name and its index in the array
 
-renderShip() {
-  const ship = this.ship;
-  const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
-  const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
-  const shipElement = document.querySelector('#ship');
-  shipElement.style.top = `${portElement.offsetTop + 28}px`;
-  shipElement.style.left = `${portElement.offsetLeft - 35}px`;
-}
+    renderShip() {
+      const ship = this.ship
+    
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+    
+      const shipElement = document.querySelector('#ship');
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+    }
+
+    setSail() {
+      const ship = this.ship
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+      if (!nextPortElement) {
+        return alert('End of the line!');
+      }
+
+      const shipElement = document.querySelector('#ship');
+const sailInterval = setInterval(() => {
+  const shipLeft = parseInt(shipElement.style.left, 10);
+  if (shipLeft === (nextPortElement.offsetLeft- 35)) {
+    ship.setSail();
+    ship.dock();
+    clearInterval(sailInterval);
+  }
+
+  shipElement.style.left = `${shipLeft + 1}px`;
+}, 20);
+    }
   }  
 
   if (typeof module !== 'undefined' && module.exports) {
