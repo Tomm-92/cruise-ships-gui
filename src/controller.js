7aa0864fc050904cf6ca1d659667bb15e2aa547b
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-01-16 21:47:49 +0000

    made changes to test files

--- src/controller.js
+++ src/controller.js
@@ -5,7 +5,7 @@
       this.initialiseSea();
       document.querySelector('#sailButton').addEventListener('click', () => {
         this.setSail();
-      });        
+      });
     }
 
     initialiseSea() {
@@ -37,38 +37,63 @@
     // the newPortElement is given a class name port and the data tags are then that ports name and its index in the array
 
     renderShip() {
-      const ship = this.ship
-    
+      const ship = this.ship;
+
       const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
-      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
-    
+      const portElement = document.querySelector(
+        `[data-port-index='${shipPortIndex}']`
+      );
+
       const shipElement = document.querySelector('#ship');
       shipElement.style.top = `${portElement.offsetTop + 32}px`;
       shipElement.style.left = `${portElement.offsetLeft - 32}px`;
     }
 
     setSail() {
-      const ship = this.ship
+      const ship = this.ship;
       const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
       const nextPortIndex = currentPortIndex + 1;
-      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+      const nextPortElement = document.querySelector(
+        `[data-port-index='${nextPortIndex}']`
+      );
       if (!nextPortElement) {
         return alert('End of the line!');
       }
 
       const shipElement = document.querySelector('#ship');
-const sailInterval = setInterval(() => {
-  const shipLeft = parseInt(shipElement.style.left, 10);
-  if (shipLeft === (nextPortElement.offsetLeft- 35)) {
-    ship.setSail();
-    ship.dock();
-    clearInterval(sailInterval);
-  }
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        if (shipLeft === nextPortElement.offsetLeft - 35) {
+          ship.setSail();
+          ship.dock();
+          clearInterval(sailInterval);
+        }
 
-  shipElement.style.left = `${shipLeft + 1}px`;
-}, 20);
+        shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20);
     }
-  }  
+
+renderMessage() {
+  const messagesElement = document.querySelector('#message');
+  messagesElement.style.width = '0px';
+
+  const messageElement = document.createElement('div');
+  messageElement.className = 'message';
+  messageElement.appendChild(messageElement);
+  const messagesElementWidth = parseInt(portsElement.style.width, 10);
+  messagesElement.style.width = `${portsElementWidth + 256}px`;
+  }
+  }
+  /* const portsElement = document.querySelector('#ports');
+  portsElement.style.width = '0px';
+  ports.forEach((port, index) => {
+    const newPortElement = document.createElement('div');
+    newPortElement.className = 'port';
+    newPortElement.dataset.portName = port.name;
+    newPortElement.dataset.portIndex = index;
+    portsElement.appendChild(newPortElement);
+    const portsElementWidth = parseInt(portsElement.style.width, 10);
+    portsElement.style.width = `${portsElementWidth + 256}px`; */
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
